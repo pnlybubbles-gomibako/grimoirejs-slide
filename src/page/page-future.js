4cb8df8f19289fbabdc8614d6ec3622b8d0fc4ac
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-02-18 15:32:06 +0900

    fix: Replaced final slide

--- src/page/page-future.js
+++ src/page/page-future.js
@@ -15,22 +15,35 @@ const fadeIn = (selector, finish) => {
     finish: finish,
   });
 }
-
+const fadeOut = (selector, finish) => {
+  return (new Tweenable()).tween({
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    duration: 1000,
+    easing: 'swifter',
+    step(state) {
+      $$(selector).setAttribute('color', new Color4(1, 1, 1, state.opacity));
+    },
+    finish: finish,
+  });
+}
 let tweenable = null;
 $$('.future').on('build', (i) => {
   switch (i) {
     case 1:
-      tweenable = fadeIn('.future .future-image0', () => {
+      tweenable = fadeIn('.future .future-image1', () => {
         tweenable = null;
       });
       break;
-    // case 2:
-    //   if (tweenable) {
-    //     tweenable.stop();
-    //     tweenable = null;
-    //   }
-    //   tweenable = fadeIn('.future .future-image1');
-    //   break;
+    case 2:
+      if (tweenable) {
+        tweenable.stop();
+        tweenable = null;
+      }
+      debugger;
+      tweenable = fadeIn('.future .future-image2');
+      //fadeOut('.future .future-image1');
+      break;
   }
 });
 
@@ -39,6 +52,6 @@ $$('.future').on('hide', () => {
     tweenable.stop();
     tweenable = null;
   }
-  $$('.future .future-image0').setAttribute('color', new Color4(1, 1, 1, 0));
-  // $$('.future .future-image1').setAttribute('color', new Color4(1, 1, 1, 0));
+  $$('.future .future-image1').setAttribute('color', new Color4(1, 1, 1, 0));
+  $$('.future .future-image2').setAttribute('color', new Color4(1, 1, 1, 0));
 });
